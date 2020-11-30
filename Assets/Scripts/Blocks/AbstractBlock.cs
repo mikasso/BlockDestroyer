5ab2253164495682c1a508e50fc70ba1ab41284a
mikasso <mikokaj.szaroleta@gmail.com>
2020-11-30 20:49:39 +0100

    First template

---
+++ Assets/Scripts/Blocks/AbstractBlock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractBlock : MonoBehaviour
+{
+    public int life;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+        void OnCollisionEnter(Collision collision)
+    {
+        life--;
+        if (life <= 0)
+            Destroy(this);
+    }
+}
---
+++ Assets/Scripts/BlocksManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocksManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject block;
+    public int amount;
+
+    void Start()
+    {
+        Instantiate(block, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}

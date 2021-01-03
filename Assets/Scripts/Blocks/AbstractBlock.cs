e2f2799c245a92b50eedd314762a07fce36b3d45
mikasso <mikokaj.szaroleta@gmail.com>
2021-01-03 19:48:35 +0100

    Mikolaj idea of particle system

--- Assets/Scripts/Blocks/AbstractBlock.cs
+++ Assets/Scripts/Blocks/AbstractBlock.cs
@@ -6,7 +6,7 @@ public class AbstractBlock : MonoBehaviour
 {
     public int life;
     public PlayerManager player;
-    
+    public GameObject blast;
     private int hitValue = 1;
     // Start is called before the first frame update
     protected virtual void Start()
@@ -20,6 +20,9 @@ public class AbstractBlock : MonoBehaviour
         player.IncreaseScore(hitValue);
         life--;
         if (life <= 0)
+        {
+            Instantiate(blast, transform.position, transform.rotation);
             Destroy(gameObject);
+        }
     }
 }

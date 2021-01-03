46cef491423e43636f44a2aaee683885769e7ce9
zwojak <kuba.zwolski11@gmail.com>
2021-01-03 19:46:55 +0100

    fonts, camera, texts

--- Assets/Scripts/Blocks/AbstractBlock.cs
+++ Assets/Scripts/Blocks/AbstractBlock.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AbstractBlock : MonoBehaviour
 {
     public int life;
     public PlayerManager player;
-    
+    public Text mytext;
+    public ParticleSystem ps;
+
     private int hitValue = 1;
     // Start is called before the first frame update
     protected virtual void Start()
     {
+       // ps.Pause();
+        mytext.text = life.ToString();
+        mytext.fontSize = 23;
+        RectTransform rt = mytext.GetComponent<RectTransform>();
+        rt.anchoredPosition = new Vector3(transform.position.x*179-525.5f, transform.position.y*179-926.0f, 0);
         GameObject obj = GameObject.Find("Player");
         player = obj.GetComponent<PlayerManager>();
     }
@@ -18,8 +26,12 @@ public class AbstractBlock : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         player.IncreaseScore(hitValue);
+        mytext.text = life.ToString();
         life--;
+       //             ps.Emit(100);
         if (life <= 0)
+        {
             Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/BlocksManager.cs
+++ Assets/Scripts/BlocksManager.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class BlocksManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first frame updateffd
     public GameObject block;
+    public ParticleSystem ps;
     public int amount;
+    public const int blocksInRow = 6;
+    public const int rows = 4;
+    public const float leftX = 0.45f;
+    public const float topY = 10.0f;
+    public const float blockSize = 1.0f;
 
     void Start()
     {
-        Instantiate(block, new Vector3(0, 6.0f, 0), Quaternion.identity);
+        ps = (ParticleSystem)Resources.Load("Prefabs/Particle System", typeof(ParticleSystem));
+        Instantiate(ps, new Vector3(3.0f,5.0f,0), Quaternion.identity);
+        ps.Stop();
+        for (int i=0; i<1; i++)
+        {
+            for(int j=0; j<1; j++)
+            {
+                Instantiate(block, new Vector3(leftX+ blockSize*j+2.5f, topY - blockSize*i-6.0f, 0), Quaternion.identity);//4x6 klocow
+            }
+        }
     }
 
     // Update is called once per frame

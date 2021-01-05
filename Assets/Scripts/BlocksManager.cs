c29659f81fb99854356d07f0de974d769563656a
zwojak <kuba.zwolski11@gmail.com>
2021-01-05 19:00:31 +0100

    Saves and loads of blocks

--- Assets/Scripts/BlocksManager.cs
+++ Assets/Scripts/BlocksManager.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class BlocksManager : MonoBehaviour
 {
@@ -56,14 +58,61 @@ public class BlocksManager : MonoBehaviour
 
     internal void SaveBlocks()
     {
-       
+       BinaryFormatter bf = new BinaryFormatter();
+	   FileStream file = File.Create(Application.persistentDataPath + "/gameInfo.dat");
+	   GameObject[] arr = GameObject.FindGameObjectsWithTag("Block");
+	   BlockData bd = new BlockData(arr.Length);
+	   foreach(GameObject blockObject in arr)
+	   {
+		   int i=0;
+		   SpriteRenderer rend = blockObject.GetComponent<SpriteRenderer>();
+		   bd.color[i,0] = rend.color[0];
+		   bd.color[i,1] = rend.color[1];
+		   bd.color[i,2] = rend.color[2];
+		   bd.color[i,3] = rend.color[3];
+		   bd.life[i] = blockObject.GetComponent<AbstractBlock>().life;
+		   bd.position[i,0] = blockObject.transform.position.x;
+		   bd.position[i,1] = blockObject.transform.position.y;
+		   i+=1;
+	   }
+	   bd.amtOfBlocks = arr.Length;
+	   bf.Serialize(file,bd);
+	   file.Close();
     }
+	
     internal void LoadBlocks()
     {
-       
-    }
+		if(File.Exists(Application.persistentDataPath + "/gameInfo.dat"))
+		{
+		   BinaryFormatter bf = new BinaryFormatter();
+		   FileStream file = File.Open(Application.persistentDataPath + "/gameInfo.dat", FileMode.Open);
+		   BlockData bd = (BlockData)bf.Deserialize(file);
+		   file.Close();
+		   for(int i=0; i<bd.amtOfBlocks; i++){
+			   AbstractBlock abstractBlock = block.GetComponent<AbstractBlock>();
+			   abstractBlock.life = bd.life[i];
+			   SpriteRenderer rend = block.GetComponent<SpriteRenderer>();
+			   rend.color = new Color(bd.color[i,0],bd.color[i,1],bd.color[i,2],bd.color[i,3]);
+			   Instantiate(block, new Vector3(bd.position[i,0],bd.position[i,1], 0), Quaternion.identity);//4x6 klocow
+		   }
+		}
+	}
     internal void ForgotBlocks()
     {
         
     }
+}
+
+[Serializable]
+class BlockData
+{
+	public int amtOfBlocks;
+	public int[] life;//
+	public float[,] color;
+	public float[,] position;
+	public BlockData(int size){
+		color = new float[size,4];
+		position = new float[size,2];
+		life = new int[size];
+	}
 }
